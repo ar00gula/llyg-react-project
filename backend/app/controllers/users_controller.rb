a079d498f209c5cba6bb2022b4b46b02cb2a1aac
ar00gula <arianna.mayer@gmail.com>
2021-03-14 12:25:24 -0400

    backend for user auth

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-    
+
+  skip_before_action :require_login, only: [:create]
+
   def create
     user = User.create(user_params)
     if user.valid?
@@ -8,19 +10,6 @@ class UsersController < ApplicationController
         render json: {user: user, jwt: token}
     else
         render json: {errors: user.errors.full_messages}, status: :not_acceptable}
-    end
-  end
-
-  def login
-    user = User.find_by(username: params[:username])
-    if user && user.authenticate(params[:password])
-      # authenticate is provided by bcrypt
-      payload = {user_id: user.id}
-      token = encode_token(payload)
-      # encode_token is a method from application controller
-      render json: {user: user, jwt: token, success: "Welcome back, #{user.username}!"}
-    else
-      render json: {failure: "Log in failed! Please check username and password."}
     end
   end
 
